1fa0df01609538a8aa746ad99d2c44a00a3f5027
Bryxny <bryonydoh2@gmail.com>
2025-10-18 18:34:28 +0100

    style: update

--- frontend/src/components/CollectionModal.tsx
+++ frontend/src/components/CollectionModal.tsx
@@ -36,6 +36,9 @@ export default function CollectionModal() {
     }
   };
 
+  const buttonClass =
+    "py-2 bg-zinc-900 text-neutral-200 border border-zinc-600 hover:border-yellow rounded-lg w-full";
+
   return (
     <>
       <div
@@ -53,20 +56,20 @@ export default function CollectionModal() {
 
       {isModalOpen && (
         <div className="fixed inset-0 bg-zinc-900/80 z-40 flex justify-end">
-          <div className="bg-zinc-900 border w-100 h-full relative flex flex-col overflow-hidden">
+          <div className="bg-zinc-900 border w-100 h-full w-full sm:w-[400px] relative flex flex-col overflow-hidden">
             <button
               className="absolute top-4 right-6 text-gray-500 hover:text-gray-300 z-50"
               onClick={() => setIsModalOpen(false)}>
               ✕
             </button>
 
-            <h2 className="text-xl m-2 p-2 text-neutral-200 relative z-10">
+            <h2 className="text-xl m-2 pb-3 p-2 text-white font-bold border-b border-yellow relative z-10">
               Your Collection
             </h2>
 
             <div className="flex-1 overflow-y-auto px-4 pt-4 pb-32 custom-scrollbar">
               {collection.length === 0 ? (
-                <p className="text-white relative z-10">
+                <p className="text-neutral-200 relative z-10">
                   No artworks selected.
                 </p>
               ) : (
@@ -74,9 +77,9 @@ export default function CollectionModal() {
                   {collection.map((art, index) => (
                     <div
                       key={art.id}
-                      className={`break-inside-avoid relative group cursor-pointer mb-4 border-3 ${
+                      className={`break-inside-avoid relative group cursor-pointer mb-4 border-2 ${
                         selected === art.id
-                          ? "border-yellow shadow-lg"
+                          ? "border-yellow"
                           : "border-transparent"
                       }`}
                       onClick={() => handleReplace(art.id)}>
@@ -121,17 +124,16 @@ export default function CollectionModal() {
                 </div>
               )}
             </div>
-
-            <div className="sticky bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent px-4 py-4 z-20">
+            <div className="sticky bottom-0 left-0 w-full bg-zinc-900/90 px-4 py-4 z-20">
               {pathname === "/exhibition" ? (
                 <button
-                  className="py-2 bg-zinc-900 text-neutral-200 border border-zinc-600 hover:border-yellow rounded-lg w-full"
+                  className={buttonClass}
                   onClick={() => router.push("/")}>
                   Edit Collection
                 </button>
               ) : (
                 <button
-                  className="py-2 bg-zinc-900 text-neutral-200 border border-zinc-600 hover:border-yellow rounded-lg w-full"
+                  className={buttonClass}
                   onClick={() => router.push("/exhibition")}>
                   Go to Exhibition
                 </button>
