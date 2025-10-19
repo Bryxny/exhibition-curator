5225087c9e17112aaba22f65bd09debc442ac9c3
Bryxny <bryonydoh2@gmail.com>
2025-10-19 17:05:31 +0100

    feat: add manifest

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,21 @@
+export default function Head() {
+  return (
+    <>
+      <title>Frame & Curate</title>
+      <meta
+        name="description"
+        content="Curate and display artworks from museums worldwide"
+      />
+
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#4e4e4eff" />
+
+      <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+    </>
+  );
+}
